6301f20b7e347cfbc2235128094e0adc8944bddc
Anu096279 <Anuwat57766@gmail.com>
2022-02-12 02:46:54 +0700

    master

---
+++ src/components/Banner.js
@@ -0,0 +1,114 @@
+import React from "react";
+import styled from "styled-components";
+function Banner() {
+   return (
+      <Banners styleclassName="banner">
+         <div className="banner-con">
+            <div className="hello-con">
+               <p>Hey, I’m</p>
+            </div>
+            <div className="user-name">
+               <p className="name">Anuwat Thisuka</p>
+            </div>
+            <div className="about-info">
+               <p>
+                  I'm a software engineer based in Toronto, Canada and also a
+                  communication and journalism student. I enjoy creating things
+                  that live on the internet, whether that be websites,
+                  applications, or anything in between. I have been freelancing
+                  for a year now while studying at the university and I've
+                  manage to gain a decent amount of experience and valuable
+                  knowledge from all different kinds of fields throughout my
+                  projects/work.
+               </p>
+            </div>
+            <div className="btn-say">
+               <li>
+                  <a href="https://anuwatthisuka.netlify.app/">Say hi --&gt;</a>
+               </li>
+            </div>
+         </div>
+      </Banners>
+   );
+}
+
+export default Banner;
+
+const Banners = styled.div`
+   background-color: #111111;
+   width: 100vw;
+   height: 85vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   .banner-con {
+      max-width: 85vw;
+      height: 80vh;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+   }
+   .hello-con {
+      display: flex;
+      width: 300px;
+      height: 60px;
+      align-items: center;
+   }
+   .hello-con p {
+      font-size: 18px;
+      color: white;
+      letter-spacing: 2px;
+      font-weight: 300px;
+   }
+   .user-name {
+      display: flex;
+      width: 85vw;
+      height: 100px;
+      align-items: center;
+   }
+   .user-name p {
+      font-size: 96px;
+      font-weight: 500;
+      letter-spacing: 2px;
+   }
+   .name {
+      background: linear-gradient(to right, #9845e8, #33d2ff, #dd5789 90%);
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+   }
+   .about-info {
+      margin-top: 20px;
+      display: flex;
+      width: 50vw;
+      height: auto;
+      align-items: center;
+   }
+   .about-info p {
+      font-size: 18px;
+      color: #f0f0f0;
+      font-weight: 300;
+   }
+   .btn-say {
+      height: 100px;
+      width: 50vw;
+      display: flex;
+      align-items: center;
+   }
+   .btn-say li {
+      height: 40px;
+      width: 50vw;
+      display: flex;
+      align-items: center;
+   }
+   .btn-say li a {
+      font-size: 24px;
+      color: #f0f0f0f0;
+      text-decoration: none;
+   }
+   .btn-say li a:hover {
+      background: linear-gradient(to right, #9845e8, #33d2ff, #dd5789 90%);
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+      transition: all 1 ease;
+   }
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+
+function Navbar() {
+   return (
+      <Containers className="navbar">
+         <div className="navbar-con">
+            <div className="navbarlogo">
+               <a href="#home">AT.</a>
+            </div>
+            <Menulink className="menu">
+               <li className="menu-con">
+                  <a href="#somthing">Project</a>
+               </li>
+               <li className="menu-con">
+                  <a href="#somthing">Resume</a>
+               </li>
+               <li className="menu-con">
+                  <a href="#somthing">Contract</a>
+               </li>
+            </Menulink>
+         </div>
+      </Containers>
+   );
+}
+
+export default Navbar;
+
+const Containers = styled.div`
+   background-color: #111111;
+   width: 100vw;
+   height: 90px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   .navbar-con {
+      width: 85vw;
+      height: 80px;
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+   }
+   .navbarlogo {
+      display: flex;
+      align-items: center;
+      width: 200px;
+      height: 80px;
+   }
+   .navbarlogo a {
+      font-size: 30px;
+      font-weight: 500;
+      color: #f0f0f0f0;
+      text-decoration: none;
+   }
+`;
+
+const Menulink = styled.div`
+   width: 350px;
+   height: 70px;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   .menu-con {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50px;
+      width: auto;
+   }
+   .menu-con a {
+      font-size: 18px;
+      font-weight: 300;
+      color: white;
+      text-decoration: none;
+   }
+   .menu-con a:hover {
+      background: linear-gradient(to right, #9845e8, #33d2ff, #dd5789 90%);
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+      transition: all 1 ease;
+   }
+`;
