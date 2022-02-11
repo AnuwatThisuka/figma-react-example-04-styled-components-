b8480436b73d656c2e4ac54476d8798180e873fb
Anu096279 <Anuwat57766@gmail.com>
2022-02-12 02:58:29 +0700

    add page experience

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Experience from "./components/Experience";
 function App() {
    return (
       <>
          <Navbar />
          <Banner />
+         <Experience />
       </>
    );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+function Experience() {
+   return (
+      <div className="experience">
+         <div className="exp-con">
+            <div className="header-exp">
+               <p>Experience</p>
+            </div>
+            <div className="exp-info">
+               <div className="exp-info-1">
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Vitae numquam eaque tempore? Voluptatum voluptas
+                     consequuntur ipsa asperiores, sequi quis cumque delectus
+                     illo labore vel, fugit ipsam maxime distinctio
+                     reprehenderit quaerat!
+                  </p>
+               </div>
+               <div className="exp-info-1">
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Vitae numquam eaque tempore? Voluptatum voluptas
+                     consequuntur ipsa asperiores, sequi quis cumque delectus
+                     illo labore vel, fugit ipsam maxime distinctio
+                     reprehenderit quaerat!
+                  </p>
+               </div>
+               <div className="exp-info-1">
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Vitae numquam eaque tempore? Voluptatum voluptas
+                     consequuntur ipsa asperiores, sequi quis cumque delectus
+                     illo labore vel, fugit ipsam maxime distinctio
+                     reprehenderit quaerat!
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Experience;
